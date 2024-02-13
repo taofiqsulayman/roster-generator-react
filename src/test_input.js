11e345ed6041742d4b0a3717ce5a769ca4df6004
Taofiq Sulayman <sulaymantaofiq@gmail.com>
2024-02-13 15:42:14 +0100

    Add new staff members and tasks

--- src/test_input.js
+++ src/test_input.js
@@ -24,6 +24,48 @@ const testData = {
         end: "12:00pm",
       },
     },
+    {
+      name: "John",
+      role: "nurse",
+      shift: {
+        start: "02:00pm",
+        end: "10:00pm",
+      },
+    },
+    {
+      name: "Emily",
+      role: "care assistant",
+      shift: {
+        start: "08:00am",
+        end: "05:00pm",
+      },
+    },
+    {
+      name: "David",
+      role: "nurse",
+      shift: {
+        start: "06:00pm",
+        end: "02:00am",
+      },
+    },
+    {
+      name: "Jane",
+      role: "care assistant",
+      shift: {
+        start: "08:00am",
+        end: "12:00pm",
+      },
+    },
+    {
+      name: "Martha",
+      role: "care assistant",
+      shift: {
+        start: "12:00pm",
+        end: "06:00pm",
+      },
+    },
+
+
   ],
 
   tasks: [
@@ -81,6 +123,37 @@ const testData = {
         time: '02:24pm',
         su: 'Mary Jane',
     },
+    {
+        supportPersonel: 'care assistant',
+        noSupportPersonel: 1,
+        time: '10:00am',
+        su: 'Martha paul',
+    },
+    {
+        supportPersonel: 'care assistant',
+        noSupportPersonel: 2,
+        time: '09:00am',
+        su: 'Mary Jane',
+    },
+    {
+        supportPersonel: 'nurse',
+        noSupportPersonel: 2,
+        time: '12:00pm',
+        su: 'Mary Jane',
+    },
+    {
+        supportPersonel: 'care assistant',
+        noSupportPersonel: 1,
+        time: '04:00pm',
+        su: 'Mary Jane',
+    },
+    {
+        supportPersonel: 'nurse',
+        noSupportPersonel: 2,
+        time: '02:24pm',
+        su: 'Mary Jane',
+    },
+
   ],
 };
 
